b87e3a5a777d434a0c6f4512c7add89f0ac2f5f8
UyenNguyen <ndtudaisy@gmail.com>
2021-11-29 09:11:38 +0700

    run auth login

---
+++ src/service/authUser.js
@@ -0,0 +1,39 @@
+import auth, { firebase } from '@react-native-firebase/auth';
+
+export const signupUser = (email, password, callback) => {
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then(user => {
+    callback ({result: "success", user})
+  })
+  .catch(err => {
+    const errorCode = err.code;
+    if (errorCode === 'auth/email-already-in-use') {
+      callback({result: "fail", error: "That email is already in use"})
+    } 
+     else if (errorCode === 'auth/invalid-email') {
+      callback({result: "fail", error: "That email address is invalid"})
+    } else {
+      callback({result: "fail", error: err})
+    }
+    console.log(err)
+  })
+}
+
+export const signinUser = (email, password, callback) => {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then(user => {
+    console.log('user: ', user)
+    callback({result: "success", user})
+  })
+  .catch(err => {
+    callback({result: "fail", error: err})
+    console.log(err)
+  })
+}
+
+export const signoutUser = () => {
+  firebase.auth().signOut()
+  .then(user => {
+    console.log("sign out successfully")
+  })
+}
