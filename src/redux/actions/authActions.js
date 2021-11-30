c2afe764079f9d6481ef87ae8ab242b6c4e9ea56
UyenNguyen <ndtudaisy@gmail.com>
2021-11-30 10:06:33 +0700

    build log out firebase

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -1,5 +1,9 @@
 import { createAction } from "@reduxjs/toolkit";
 
+export const signupRequest = createAction('auth/loginRequest');
+export const signupSuccess = createAction('auth/loginSuccess');
+export const signFailed = createAction('auth/loginFailed');
+
 export const loginRequest = createAction('auth/loginRequest');
 export const loginSuccess = createAction('auth/loginSuccess');
 export const loginFailed = createAction('auth/loginFailed');
